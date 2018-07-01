2242de04c9c276d4f81e2ab25caa55d792341208
Manuel Marinez <marinezdev@gmail.com>
2018-07-01 11:04:03 -0400

    Agregar metodo de saludo

---
+++ src/Menu.java
@@ -0,0 +1,14 @@
+public class Menu {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.saludo();
+    }
+
+    public void saludo() {
+        System.out.println("          Bienvenido\n" +
+                "Por favor inserte su tajeta ATM");
+    }
+
+}
+
+
