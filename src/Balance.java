3da9765960b7bc9f28e6255269d8213008d45c3c
mmarinez <manuel.marinez@verizon.com>
2018-07-02 12:39:14 -0400

    Agregar Clase balance y metodo de seleccion de menu

---
+++ src/Balance.java
@@ -0,0 +1,13 @@
+public class Balance {
+
+    private int cuentaBalance;
+
+    public Balance(){
+        cuentaBalance = 10000;
+    }
+
+    public void mostrarBalance(){
+        System.out.println("Su balance actual es: " + cuentaBalance);
+    }
+
+}
--- src/Menu.java
+++ src/Menu.java
@@ -1,12 +1,30 @@
+import java.util.Scanner;
+
 public class Menu {
+    private Balance balance;
+
     public static void main(String[] args) {
         Menu menu = new Menu();
-        menu.saludo();
+        menu.seleccion();
     }
 
-    private void saludo() {
-        System.out.println("          Bienvenido\n" +
-                "Por favor inserte su tajeta ATM");
+    private void seleccion() {
+        Scanner scanner = new Scanner(System.in);
+        int operacion = 0;
+        System.out.println("Elija la transaccion que desea efectuar:\n");
+        System.out.println("1- Balance");
+        System.out.println("2- Retiro");
+        System.out.println("3- Deposito");
+
+        operacion = scanner.nextInt();
+
+        switch (operacion){
+
+        case 1:
+            balance = new Balance();
+            balance.mostrarBalance();
+            break;
+        }
     }
 
 }
