748b304e8c6ddc5e45b338865655477defb6f74f
Manuel Marinez <marinezdev@gmail.com>
2018-07-04 23:03:47 -0400

    Agregar autentificacion

--- src/Menu.java
+++ src/Menu.java
@@ -1,3 +1,6 @@
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Menu {
@@ -5,7 +8,42 @@ public class Menu {
 
     public static void main(String[] args) throws Exception {
         Menu menu = new Menu();
+        Scanner scanner = new Scanner(System.in);
+        for (int run = 0; run < 3; run++) {
+            System.out.println("Ingrese su tarjeta");
+            String acctNum = scanner.nextLine();
+            System.out.println("Ingrese su PIN");
+            String pwd = scanner.nextLine();
+
+            String result = menu.checkID(acctNum, pwd);
+            if (!result.equals("ERROR")) {
+                break;
+            } else if (run == 2) {// you cannot try to log in anymore than 3
+                // times
+                System.out.println("MAXIMO DE INTENTOS EXCEDIDO");
+                return;
+            }
+
+        }
         menu.seleccion();
+    }
+
+    private BufferedReader readFileAsString(String fileName) throws FileNotFoundException {
+
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(new File(fileName)));
+        return bufferedReader;
+    }
+
+    private String checkID(String acctNum, String pwd) throws Exception {
+        String result = "ERROR";
+        BufferedReader data = readFileAsString("C:\\Users\\USER\\IdeaProjects\\atm-project\\credentiales\\usuario.txt");
+
+        if (acctNum.contains(data.readLine()) && pwd.contains(data.readLine())) {
+            result = "520.36";
+        }
+
+        System.out.println(result);
+        return result;
     }
 
     protected void seleccion() throws Exception {
